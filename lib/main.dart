07ea7e791e3eb8f656f8152622d5ed4d04cc0902
Kalle Johansson <kallej1johansson@gmail.com>
2020-11-21 15:29:25 +0100

    setup: firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,11 +16,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        backgroundColor: Color(0xfff1f9ff),
       ),
-      home: Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, userSnapshot) {
+          // Show splash screen while checking if user is signed in.
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold();
+          }
+          var signedIn = userSnapshot.hasData;
+          return signedIn ? Scaffold() : Scaffold();
+        },
       ),
     );
   }
