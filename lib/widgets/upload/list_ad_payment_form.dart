2687ba5a8924e085d791d7a56a2303528ad1f49b
Kalle Johansson <kallej1johansson@gmail.com>
2020-12-11 17:50:10 +0100

    feat: upload list ad form

--- lib/widgets/upload/list_ad_payment_form.dart
+++ lib/widgets/upload/list_ad_payment_form.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'ad_form_title.dart';
-import 'ad_form_phone_field.dart';
 import 'upload_step.dart';
 import '../../lang/my_localizations.dart';
 
-class FindAdUploaderForm extends UploadStep {
+class ListAdPaymentForm extends UploadStep {
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -19,13 +18,12 @@ class FindAdUploaderForm extends UploadStep {
 
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          AdFormTitle(l10n.findAdTermsTitle),
-          Text(l10n.longTemp),
-          AdFormTitle(l10n.findAdTermsPhone),
-          AdFormPhoneField(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            AdFormTitle(l10n.listAdPaymentTitle),
+          ],
+        ),
       ),
     );
   }
