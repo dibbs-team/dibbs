6fa671322048839e93cc6617d70fe42df8cc7493
Kalle Johansson <kallej1johansson@gmail.com>
2021-02-24 17:52:34 +0100

    refactor: generalize divider theme

--- lib/design/app_theme.dart
+++ lib/design/app_theme.dart
@@ -33,9 +33,8 @@ class AppTheme {
           type: BottomNavigationBarType.fixed,
         ),
         dividerTheme: DividerThemeData(
-          thickness: 2.0,
-          indent: 10.0,
-          endIndent: 10.0,
+          thickness: 1.0,
+          space: 36.0,
         ),
         appBarTheme: AppBarTheme(
           color: MyColors.white,
